a5a3951e8998e3f4cd916e7bce033c505270e10a
twistednarcisis <123904889+twistednarcisis@users.noreply.github.com>
2025-06-01 10:31:39 +1000

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+const firebaseConfig = {
+
+  apiKey: "AIzaSyCzBqOhwDgwcvqeAOK51-ZV6tgiibzqKR0",
+
+  authDomain: "tattoo-do-db.firebaseapp.com",
+
+  projectId: "tattoo-do-db",
+
+  storageBucket: "tattoo-do-db.firebasestorage.app",
+
+  messagingSenderId: "880236910240",
+
+  appId: "1:880236910240:web:f7602760557d0e3d45dbc5"
+
+};
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+3. Create src/components/Login.js
